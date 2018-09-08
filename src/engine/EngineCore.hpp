e26c93dd9d9fb2c2f9a6fb91788eb2ff85ac9fa8
cwduncan281 <cwduncan281@gmail.com>
2018-09-08 18:14:30 -0500

    beginnings of the project architecture

---
+++ src/engine/EngineCore.hpp
@@ -0,0 +1,14 @@
+#ifndef ENGINE_ENGINECORE_HPP
+#define ENGINE_ENGINECORE_HPP
+
+#include <SDL2/SDL.h>
+
+namespace engine {
+
+class EngineCore {
+
+};
+
+} // namespace engine
+
+#endif // ENGINE_ENGINECORE_HPP
---
+++ src/engine/Scene.hpp
@@ -0,0 +1,12 @@
+#ifndef ENGINE_SCENE_HPP
+#define ENGINE_SCENE_HPP
+
+namespace engine {
+
+class Scene {
+
+};
+
+} // namespace engine
+
+#endif // ENGINE_SCENE_HPP
