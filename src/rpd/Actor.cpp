a3b2f13b8bcc6436cffcbe438e36b17df2e95a4c
cwduncan281 <cwduncan281@gmail.com>
2018-09-29 18:34:31 -0500

    minor improvement to movement animation direction

--- src/rpd/Actor.cpp
+++ src/rpd/Actor.cpp
@@ -3,7 +3,7 @@
 namespace rpd {
 
 Actor::Actor(double pos_x, double pos_y) :
-      pos_x_(pos_x), pos_y_(pos_y), max_velocity_(1),
+      pos_x_(pos_x), pos_y_(pos_y), max_velocity_(0.1),
       renderable_(std::make_unique<engine::Renderable>("../assets/archer.png", pos_x, pos_y)) {
    // lol idunno
 }
@@ -23,16 +23,22 @@ Actor& Actor::operator=(const Actor& rhs) {
 void Actor::ReceiveInput(engine::InputData input) {
    if(input.input_type == engine::InputType::MOVE_UP) {
       vel_y_ -= max_velocity_ * input.input_value;
-      renderable_->SetAnimation(3);
    } else if(input.input_type == engine::InputType::MOVE_DOWN) {
       vel_y_ += max_velocity_ * input.input_value;
-      renderable_->SetAnimation(0);
    } else if(input.input_type == engine::InputType::MOVE_LEFT) {
       vel_x_ -= max_velocity_ * input.input_value;
-      renderable_->SetAnimation(1);
    } else if(input.input_type == engine::InputType::MOVE_RIGHT) {
       vel_x_ += max_velocity_ * input.input_value;
+   }
+
+   if(vel_x_ > 0) {
       renderable_->SetAnimation(2);
+   } else if(vel_x_ < 0) {
+      renderable_->SetAnimation(1);
+   } else if(vel_y_ < 0) {
+      renderable_->SetAnimation(3);
+   } else if(vel_y_ > 0) {
+      renderable_->SetAnimation(0);
    }
 }
 
